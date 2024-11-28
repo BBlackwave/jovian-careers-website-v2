9a6c929177abeefd1fa0e2f365d1408ea06392f3
BBlackwave <125770746+BBlackwave@users.noreply.github.com>
2024-11-27 19:00:21 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = "mysql+pymysql://pg6lm1rrbl96ryj8h7j5:pscale_pw_RmvDZn1FR4o6U7E45Fi9UJU9BdS2Bp1Vxto4hMSyFPY@aws.connect.psdb.cloud/joviancareers?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://7boscano320b1yx4waix:pscale_pw_ei2pxbhTuepSDb65JBvclzhMFOe87n0pLYKV7mo27TrY@aws.connect.psdb.cloud/joviancareers?charset=utf8mb4"
 
 engine = create_engine(
   db_connection_string,
