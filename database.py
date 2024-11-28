6ca4ef939d7c71ceb28af11f9bed855db2f4db26
BBlackwave <125770746+BBlackwave@users.noreply.github.com>
2024-11-28 00:12:12 +0000

    changes

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://pg6lm1rrbl96ryj8h7j5:pscale_pw_RmvDZn1FR4o6U7E45Fi9UJU9BdS2Bp1Vxto4hMSyFPY@aws.connect.psdb.cloud/joviancareers?charset=utf8mb4"
 
 engine = create_engine(
   db_connection_string,
