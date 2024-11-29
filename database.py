f3a94a15fbbe845b8a575c3394d41c5c289eee57
BBlackwave <125770746+BBlackwave@users.noreply.github.com>
2024-11-29 18:31:05 +0000

    Update 2

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
 
-"""
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+db_connection_string = os.environ["DB_CONNECTION_STRING"]
 
 
 engine = create_engine(
@@ -12,14 +12,7 @@ engine = create_engine(
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
-"""
-db_connection_string = os.getenv("DB_CONNECTION_STRING")
 
-ssl_args = {'ssl': {
-            'ssl_ca': "/etc/ssl/cert.pem"}
-          }
-engine = create_engine(db_connection_string,
-                      connect_args=ssl_args)
 
 def load_jobs_from_db():
   with engine.connect() as conn:
