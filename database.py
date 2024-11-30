3f7b1f7b0eb40485b7343fb3d9a8f398a5df5071
BBlackwave <125770746+BBlackwave@users.noreply.github.com>
2024-11-30 22:57:24 +0000

    Made pages dynamic

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
 
+from sqlalchemy.engine.interfaces import ReflectedForeignKeyConstraint
+
 
 db_connection_string = os.environ["DB_CONNECTION_STRING"]
 
@@ -21,3 +23,19 @@ def load_jobs_from_db():
     for row in result.mappings().all():
       jobs.append(dict(row))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result =conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    row = result.fetchone()
+    if row:
+      return row._asdict()
+    else:
+      return None
+      
+    
+                         
+    
