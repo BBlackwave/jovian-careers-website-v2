bcc99f33665d08d0110e9c051e407447bbd0261a
BBlackwave <125770746+BBlackwave@users.noreply.github.com>
2024-12-02 15:39:24 +0000

    API push

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
 
-from sqlalchemy.engine.interfaces import ReflectedForeignKeyConstraint
 
 
 db_connection_string = os.environ["DB_CONNECTION_STRING"]
@@ -36,6 +35,23 @@ def load_job_from_db(id):
     else:
       return None
       
-    
-                         
-    
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text(
+          "INSERT INTO applications(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+      )
+
+      conn.execute(
+          query,
+          {
+              "job_id": job_id,
+              "full_name": data["full_name"],
+              "email": data["email"],
+              "linkedin_url": data["linkedin_url"],
+              "education": data["education"],
+              "work_experience": data["work_experience"],
+              "resume_url": data["resume_url"],
+          },
+        )
+      conn.commit()
+  
